0c9fba9e18b5540c9a1ffd0018af3f9b228c91ef
Junqi Liao <junqiliao@Junqis-MBP.fios-router.home>
2018-10-09 10:45:17 -0400

    clean up the experimental files and move the func to index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App sort={"gold"}/>, document.getElementById('root'));
+// export const initialize = (element_id, sort="gold") => {
+  // ReactDOM.render(<App sort={sort} />, element_id);
+// }
+
+// TODO: need to take these two as params
+const sort = "gold";
+const element_id = "medal-widget"
+
+ReactDOM.render(<App sort={sort}/>, document.getElementById(element_id));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
