4cdde4665f7f084cf6030467f3bebc0ae34aabc5
Junqi Liao <junqiliao@Junqis-MBP.fios-router.home>
2018-10-11 00:49:38 -0400

    wrap the app in an iframe

--- src/index.js
+++ src/index.js
@@ -2,19 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-// export const initialize = (element_id, sort="gold") => {
-  // ReactDOM.render(<App sort={sort} />, element_id);
-// }
+// TODO: simple, but doesn't work in IE
+const urlParams = new URLSearchParams(window.location.search);
+const sort = urlParams.get("sort");
 
-// TODO: need to take these two as params
-const sort = "gold";
-const element_id = "medal-widget"
-
-ReactDOM.render(<App sort={sort}/>, document.getElementById(element_id));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App sort={sort}/>, document.getElementById("root"));
